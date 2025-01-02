11ed14b936988b6edf1643501fdd3c2a45afa0f7
hby <boye.hou@advancegroup.com>
2025-01-02 15:55:21 +0800

    init
    init

---
+++ SwiftUiMultiplatformBaseProject/SwiftUiMultiplatformBaseProject/SwiftUiMultiplatformBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMultiplatformBaseProjectApp.swift
+//  SwiftUiMultiplatformBaseProject
+//
+//  Created by BY H on 2025/1/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMultiplatformBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUiMultiplatformBaseProject/SwiftUiMultiplatformBaseProjectTests/SwiftUiMultiplatformBaseProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUiMultiplatformBaseProjectTests.swift
+//  SwiftUiMultiplatformBaseProjectTests
+//
+//  Created by BY H on 2025/1/2.
+//
+
+import Testing
+
+struct SwiftUiMultiplatformBaseProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
